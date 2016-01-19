e9780500cc1203d5cfc20144fd2c1bb934a30cf2
Subham <sbhm94@gmail.com>
2016-01-19 16:52:04 +0530

    update 1

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get install espeak
 sudo pip install --upgrade --ignore-installed slate==0.3 pdfminer==20110515
-
+sudo pip install pyttsx
--- pdf2speech.py
+++ pdf2speech.py
@@ -1,17 +1,25 @@
-import slate,subprocess
+import slate,subprocess,pyttsx
 loc=raw_input('Enter location of pdf:')
 with open(loc) as f:
-    doc=slate.PDF(f)
-page=input('Enter page number to save(in 1 file):')
-if page==0:
-    page=len(doc)
-i=0
-while i<len(doc):
-    t=""
-    for j in range(i,i+page):
-        if i+j>=len(doc):
-            break
-        t=t+doc[j-1]
-    name=(str)(i)+" to "+(str)(i+page)
-    i=i+page
-    subprocess.call(["espeak", "-w"+name+".mp3", t])
+        doc=slate.PDF(f)
+choice=input("Speak(0)/Save(1):")
+if choice==1:
+        page=input('Enter number of to save in 1 file(0 for all):')
+        if page==0:
+                page=len(doc)
+        i=0
+        while i<len(doc):
+                t=""
+                for j in range(i,i+page):
+                        if i+j>=len(doc):
+                                break
+                        t=t+doc[j-1]
+                name=(str)(i)+" to "+(str)(i+page)
+                i=i+page
+                subprocess.call(["espeak", "-w"+name+".mp3", t])
+else:
+        page=input("Enter page number to speak:")
+        t=doc[page-1]
+        engine=pyttsx.init()
+        engine.say(doc[page-1])
+        engine.runAndWait()
